7ead7c044fbca7f2ef63910240bf63b21148e7e4
IvanovaNataly <letter4natalia@gmail.com>
2021-01-11 18:36:29 +0200

    reactive service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,7 @@ import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareRep
 import {HttpClient} from '@angular/common/http';
 import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import {CoursesService} from '../services/couses.service';
 
 
 @Component({
@@ -14,29 +15,29 @@ import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 })
 export class HomeComponent implements OnInit {
 
-  beginnerCourses: Course[];
+  beginnerCourses$: Observable<Course[]>;
 
-  advancedCourses: Course[];
+  advancedCourses$: Observable<Course[]>;
 
 
-  constructor(private http: HttpClient, private dialog: MatDialog) {
+  constructor(private http: HttpClient, private dialog: MatDialog,
+              private coursesService: CoursesService) {
 
   }
 
   ngOnInit() {
 
-    this.http.get('/api/courses')
-      .subscribe(
-        res => {
+    const courses$ = this.coursesService.loadAllCourses;
 
-          const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
+    this.beginnerCourses$ = courses$()
+      .pipe(
+        map(courses => courses.filter(course => course.category === 'BEGINNER'))
+      );
 
-          this.beginnerCourses = courses.filter(course => course.category == "BEGINNER");
-
-          this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
-
-        });
-
+    this.advancedCourses$ = courses$()
+      .pipe(
+        map(courses => courses.filter(course => course.category === 'ADVANCED'))
+      );
   }
 
   editCourse(course: Course) {
